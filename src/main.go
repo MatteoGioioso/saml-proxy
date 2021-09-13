740325fae30e0f724ecab8ae3456d796e595985f
MatteoGioioso <gioioso.matteo@gmail.com>
2021-09-13 13:02:12 +0700

    progress on redirect

--- src/main.go
+++ src/main.go
@@ -5,29 +5,58 @@ import (
 	"github.com/crewjam/saml/samlsp"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
-func mainHandler(samlSP RouteGenerator) func(w http.ResponseWriter, r *http.Request) {
+func genericHandler(generator RouteGenerator, samlSP *samlsp.Middleware) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", samlsp.AttributeFromContext(r.Context(), "cn"))
-	}
-}
+		fmt.Println(r.URL.String())
+		if r.URL.Path == "/saml/auth" {
+			fmt.Printf("%+v\n", r.Header)
+			session, err := samlSP.Session.GetSession(r)
+			if session != nil {
+				w.WriteHeader(200)
+				return
+			}
 
-func genericHandler(generator RouteGenerator, app http.HandlerFunc) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		samlSP, err := generator.CreateSamlService(r.Host)
-		if err != nil {
-			log.Fatal(err)
+			if err == samlsp.ErrNoSession {
+				w.WriteHeader(401)
+				return
+			}
+
+			return
+		}
+
+		if r.URL.Path == "/saml/login" {
+			u := &url.URL{
+				Scheme:      "http",
+				Opaque:      "",
+				User:        nil,
+				Host:        "mycoolsaml:8080",
+				Path:        "/",
+				RawPath:     "",
+				ForceQuery:  false,
+				RawQuery:    "",
+				Fragment:    "",
+				RawFragment: "",
+			}
+			r.URL = u
+			samlSP.HandleStartAuthFlow(w, r)
+			return
 		}
 
 		if r.URL.Path == "/saml/acs" {
+			trackedRequestIndex := r.Form.Get("RelayState")
+			fmt.Println(trackedRequestIndex)
 			samlSP.ServeHTTP(w, r)
 			return
 		}
 
-		account := samlSP.RequireAccount(app)
-		account.ServeHTTP(w, r)
+		if r.URL.Path == "/saml/metadata" {
+			samlSP.ServeHTTP(w, r)
+			return
+		}
 	}
 }
 
@@ -36,8 +65,13 @@ func main() {
 		MetadataEndpoint: os.Getenv("SAML_METADATA_ENDPOINT"),
 	}
 
-	app := http.HandlerFunc(mainHandler(generator))
-	handler := http.HandlerFunc(genericHandler(generator, app))
+	samlSP, err := generator.CreateSamlService("localhost:9000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//app := http.HandlerFunc(mainHandler(generator))
+	handler := http.HandlerFunc(genericHandler(generator, samlSP))
 
 	if err := http.ListenAndServe(":9000", handler); err != nil {
 		log.Fatal(err)
--- src/routes_generator.go
+++ src/routes_generator.go
@@ -5,11 +5,15 @@ import (
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"fmt"
 	"github.com/crewjam/saml/samlsp"
 	"net/http"
 	"net/url"
+	"os"
 )
+
+type samlHosts []string
 
 type RouteGenerator struct {
 	MetadataEndpoint string
@@ -54,4 +58,46 @@ func (g RouteGenerator) CreateSamlService(domain string) (*samlsp.Middleware, er
 	}
 
 	return samlSP, nil
+}
+
+func (g RouteGenerator) IsAllowedHost(host string) (bool, error) {
+	hostsFromEnv := os.Getenv("SAML_HOSTS")
+	hosts := samlHosts{}
+
+	if err := json.Unmarshal([]byte(hostsFromEnv), &hosts); err != nil {
+		return false, err
+	}
+
+	return contains(hosts, host), nil
+}
+
+func (g RouteGenerator) MakeRedirectUrl(r *http.Request) string {
+	fmt.Printf("%+v\n", r.Header)
+	protocol := r.Header.Get("x-forwarded-for")
+	u := url.URL{
+		Scheme:      protocol,
+		Opaque:      r.URL.Opaque,
+		User:        r.URL.User,
+		Host:        r.Host,
+		Path:        r.RequestURI,
+		RawPath:     r.URL.RawPath,
+		ForceQuery:  r.URL.ForceQuery,
+		RawQuery:    r.URL.RawQuery,
+		Fragment:    r.URL.Fragment,
+		RawFragment: r.URL.RawFragment,
+	}
+
+	fmt.Println(u.String())
+	fmt.Println(r.URL.String())
+
+	return u.String()
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
